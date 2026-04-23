f6bfba212e67874be35dabd536962abc4d2db701
Manish Kafle <209854664+CodedByManish@users.noreply.github.com>
2026-04-23 13:47:18 +0545

    Include head.php in sell_property.php

--- sell_property.php
+++ sell_property.php
@@ -12,6 +12,7 @@ if (!isset($_SESSION['userRole']) || $_SESSION['userRole'] !== 'seller') {
 <head>
     <meta charset="UTF-8">
     <title>Sell Property - HamroGharJajja</title>
+    <?php include('head.php'); ?>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
