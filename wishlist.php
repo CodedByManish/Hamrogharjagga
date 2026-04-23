87ce619a5b7ba81124025b30b391e070383d325b
Manish Kafle <209854664+CodedByManish@users.noreply.github.com>
2026-04-23 13:48:01 +0545

    Include head.php in wishlist.php

--- wishlist.php
+++ wishlist.php
@@ -46,6 +46,7 @@ $wishlist_count = $wishlist_count_result->fetch_assoc()['wishlist_count'];
 <head>
     <meta charset="UTF-8">
     <title>My Wishlist - HamroGharJajja</title>
+    <?php include('head.php'); ?>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
