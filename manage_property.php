4342123b336eee0b8c55e012b2022bd9b39f11a0
Manish Kafle <209854664+CodedByManish@users.noreply.github.com>
2026-04-23 13:46:52 +0545

    Update manage_property.php

--- manage_property.php
+++ manage_property.php
@@ -132,6 +132,7 @@ $properties = $stmt->get_result();
 <head>
     <meta charset="UTF-8">
     <title>Manage Properties - HamroGharJajja</title>
+    <?php include('head.php'); ?>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
