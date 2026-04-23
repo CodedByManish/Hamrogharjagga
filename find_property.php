2fb756c060ebe8d2e23132f36091aa528f3e04d8
Manish Kafle <209854664+CodedByManish@users.noreply.github.com>
2026-04-23 13:42:46 +0545

    Update find_property.php

--- find_property.php
+++ find_property.php
@@ -44,6 +44,7 @@ while ($row = $wishlist_items_result->fetch_assoc()) {
 <head>
     <meta charset="UTF-8">
     <title>Find Your Dream Property - HamroGharJajja</title>
+    <?php include('head.php'); ?>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
